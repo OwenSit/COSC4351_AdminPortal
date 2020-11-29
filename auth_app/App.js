281e3e863771a02ad1fe2b125ed53d9296b0b73f
SongwenXue <60166898+OwenSit@users.noreply.github.com>
2020-11-28 23:55:22 -0600

    Updated App.js

--- auth_app/App.js
+++ auth_app/App.js
@@ -5,17 +5,14 @@
  * @format
  * @flow strict-local
  */
-
+import 'react-native-gesture-handler';
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
+import {Button} from 'react-native-paper';
+import {TextInput} from 'react-native-paper';
+import {SafeAreaView, StyleSheet, ScrollView, View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {NavigationNativeContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import {
   Header,
   LearnMoreLinks,
@@ -23,92 +20,109 @@ import {
   DebugInstructions,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import LoginScreen from './android/screens/LoginScreen';
+import HomeScreen from './android/screens/HomeScreen';
+import LoadingScreen from './android/screens/LoadingScreen';
 
-const App: () => React$Node = () => {
+const Stack = createStackNavigator();
+
+const App = () => {
+  const Stack = createStackNavigator();
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Howdy!" component={LoginScreen} />
+        <Stack.Screen name="loading" component={LoadingScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+// const App: () => React$Node = () => {
+//   return (
+//     <>
+//       <StatusBar barStyle="dark-content" />
+//       <SafeAreaView>
+//         <ScrollView
+//           contentInsetAdjustmentBehavior="automatic"
+//           style={styles.scrollView}>
+//           <Header />
+//           {global.HermesInternal == null ? null : (
+//             <View style={styles.engine}>
+//               <Text style={styles.footer}>Engine: Hermes</Text>
+//             </View>
+//           )}
+//           <View style={styles.body}>
+//             <View style={styles.sectionContainer}>
+//               <Text style={styles.sectionTitle}>Step One</Text>
+//               <Text style={styles.sectionDescription}>
+//                 Edit <Text style={styles.highlight}>App.js</Text> to change this
+//                 screen and then come back to see your edits.
+//               </Text>
+//             </View>
+//             <View style={styles.sectionContainer}>
+//               <Text style={styles.sectionTitle}>See Your Changes</Text>
+//               <Text style={styles.sectionDescription}>
+//                 <ReloadInstructions />
+//               </Text>
+//             </View>
+//             <View style={styles.sectionContainer}>
+//               <Text style={styles.sectionTitle}>Debug</Text>
+//               <Text style={styles.sectionDescription}>
+//                 <DebugInstructions />
+//               </Text>
+//             </View>
+//             <View style={styles.sectionContainer}>
+//               <Text style={styles.sectionTitle}>Learn More</Text>
+//               <Text style={styles.sectionDescription}>
+//                 Read the docs to discover what to do next:
+//               </Text>
+//             </View>
+//             <LearnMoreLinks />
+//           </View>
+//         </ScrollView>
+//       </SafeAreaView>
+//     </>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   scrollView: {
+//     backgroundColor: Colors.lighter,
+//   },
+//   engine: {
+//     position: 'absolute',
+//     right: 0,
+//   },
+//   body: {
+//     backgroundColor: Colors.white,
+//   },
+//   sectionContainer: {
+//     marginTop: 32,
+//     paddingHorizontal: 24,
+//   },
+//   sectionTitle: {
+//     fontSize: 24,
+//     fontWeight: '600',
+//     color: Colors.black,
+//   },
+//   sectionDescription: {
+//     marginTop: 8,
+//     fontSize: 18,
+//     fontWeight: '400',
+//     color: Colors.dark,
+//   },
+//   highlight: {
+//     fontWeight: '700',
+//   },
+//   footer: {
+//     color: Colors.dark,
+//     fontSize: 12,
+//     fontWeight: '600',
+//     padding: 4,
+//     paddingRight: 12,
+//     textAlign: 'right',
+//   },
+// });
 
 export default App;
--- auth_server/routes/authRoutes.js
+++ auth_server/routes/authRoutes.js
@@ -1,95 +1,95 @@
-const express = require('express')
-const mongoose = require('mongoose')
+const express = require("express");
+const mongoose = require("mongoose");
 const router = express.Router();
-const Account = mongoose.model('Account');
-const Admin = mongoose.model('Admin');
-const Finance = mongoose.model('Finance');
-const Hr = mongoose.model('Hr');
-const Sales = mongoose.model('Sales');
-const Technology = mongoose.model('Technology');
+const Account = mongoose.model("Account");
+const Admin = mongoose.model("Admin");
+const Finance = mongoose.model("Finance");
+const Hr = mongoose.model("Hr");
+const Sales = mongoose.model("Sales");
+const Technology = mongoose.model("Technology");
 
-router.post('/signup',async(req,res)=>{
-    console.log(req.body)
+router.post("/signup", async (req, res) => {
+  console.log(req.body);
 
-    const {username,password,role} = req.body
+  const { username, password, role } = req.body;
 
-    try{
-        const account = new Account({username,password,role});
-        await account.save();
-        res.send('create new account succeed')
-    }catch(err){
-        res.status(422).send(err.message)
-    }   
-})
+  try {
+    const account = new Account({ username, password, role });
+    await account.save();
+    res.send("create new account succeed");
+  } catch (err) {
+    res.status(422).send(err.message);
+  }
+});
 
-router.post('/admin',async(req,res)=>{
-    console.log(req.body)
+router.post("/admin", async (req, res) => {
+  console.log(req.body);
 
-    const {linkname} = req.body
+  const { linkname } = req.body;
 
-    try{
-        const admin = new Admin({linkname});
-        await admin.save();
-        res.send('create Admin links succeed')
-    }catch(err){
-        res.status(422).send(err.message)
-    }   
-})
+  try {
+    const admin = new Admin({ linkname });
+    await admin.save();
+    res.send("create Admin links succeed");
+  } catch (err) {
+    res.status(422).send(err.message);
+  }
+});
 
-router.post('/finance',async(req,res)=>{
-    console.log(req.body)
+router.post("/finance", async (req, res) => {
+  console.log(req.body);
 
-    const {linkname} = req.body
+  const { linkname } = req.body;
 
-    try{
-        const finance = new Finance({linkname});
-        await finance.save();
-        res.send('create Finance links succeed')
-    }catch(err){
-        res.status(422).send(err.message)
-    }   
-})
+  try {
+    const finance = new Finance({ linkname });
+    await finance.save();
+    res.send("create Finance links succeed");
+  } catch (err) {
+    res.status(422).send(err.message);
+  }
+});
 
-router.post('/hr',async(req,res)=>{
-    console.log(req.body)
+router.post("/hr", async (req, res) => {
+  console.log(req.body);
 
-    const {linkname} = req.body
+  const { linkname } = req.body;
 
-    try{
-        const hr = new Hr({linkname});
-        await hr.save();
-        res.send('create Hr links succeed')
-    }catch(err){
-        res.status(422).send(err.message)
-    }   
-})
+  try {
+    const hr = new Hr({ linkname });
+    await hr.save();
+    res.send("create Hr links succeed");
+  } catch (err) {
+    res.status(422).send(err.message);
+  }
+});
 
-router.post('/sales',async(req,res)=>{
-    console.log(req.body)
+router.post("/sales", async (req, res) => {
+  console.log(req.body);
 
-    const {linkname} = req.body
+  const { linkname } = req.body;
 
-    try{
-        const sales = new Sales({linkname});
-        await sales.save();
-        res.send('create Sales links succeed')
-    }catch(err){
-        res.status(422).send(err.message)
-    }   
-})
+  try {
+    const sales = new Sales({ linkname });
+    await sales.save();
+    res.send("create Sales links succeed");
+  } catch (err) {
+    res.status(422).send(err.message);
+  }
+});
 
-router.post('/technology',async(req,res)=>{
-    console.log(req.body)
+router.post("/technology", async (req, res) => {
+  console.log(req.body);
 
-    const {linkname} = req.body
+  const { linkname } = req.body;
 
-    try{
-        const technology = new Technology({linkname});
-        await technology.save();
-        res.send('create Technology links succeed')
-    }catch(err){
-        res.status(422).send(err.message)
-    }   
-})
+  try {
+    const technology = new Technology({ linkname });
+    await technology.save();
+    res.send("create Technology links succeed");
+  } catch (err) {
+    res.status(422).send(err.message);
+  }
+});
 
-module.exports = router
+module.exports = router;
