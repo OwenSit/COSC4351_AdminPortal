0d0df197517f01e51156fd987d30dcce368601b6
SongwenXue <60166898+OwenSit@users.noreply.github.com>
2020-11-30 00:08:49 -0600

    Update FinanceScreen.js

--- auth_app/android/screens/FinanceScreen.js
+++ auth_app/android/screens/FinanceScreen.js
@@ -1,76 +1,81 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-import 'react-native-gesture-handler';
-import React, {useState} from 'react';
-import {Button} from 'react-native-paper';
-import {TextInput} from 'react-native-paper';
+import React, {Component} from 'react';
+
 import {
   StyleSheet,
   View,
   Text,
-  StatusBar,
-  KeyboardAvoidingView,
+  ScrollView,
+  Platform,
+  Alert,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-// import {accessibilityProps} fro m 'react-native-paper/lib/typescript/src/components/MaterialCommunityIcon';
+// const links = [];
+var link;
+let array = [];
+fetch('http://10.0.2.2:3000/showLinks', {
+  method: 'POST',
+  headers: {
+    'content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    role: 'Finance',
+  }),
+})
+  .then((res) => res.json())
+  .then((data) => {
+    // let array = [];
+    console.log(data);
+    for (link of data) {
+      console.log(link.linkname);
+      array.push(link.linkname);
+      console.log(array);
+    }
+  });
+console.log(array);
 
-const FinanceScreen = (props) => {
-  return (
-    <>
-      <KeyboardAvoidingView behavior="position">
-        <StatusBar backgroundColor="blue" barStyle="light-content" />
-        <Text
-          style={{
-            fontSize: 25,
-            marginTop: 10,
-            textAlign: 'center',
-          }}>
-          Welcome, Finance Admin
-        </Text>
-        <Text>{'     '}</Text>
-        <Text style={{fontSize: 25, textAlign: 'center'}}>
-          {'Available Links:'}
-        </Text>
-        <Text>{'     '}</Text>
-        <Button
-          mode="contained"
-          style={{marginLeft: 18, marginRight: 18, marginTop: 18}}
-          onPress={() => console.log('Link to Finance Reports')}>
-          Finance Reports
-        </Button>
-        <Button
-          mode="contained"
-          style={{marginLeft: 18, marginRight: 18, marginTop: 18}}
-          onPress={() => console.log('Link to Accounts Payable')}>
-          Accounts Payable
-        </Button>
-        <Button
-          mode="contained"
-          style={{marginLeft: 18, marginRight: 18, marginTop: 18}}
-          onPress={() => console.log('Link to Accounts Receivable')}>
-          Accounts Receivable
-        </Button>
-        <Button
-          mode="contained"
-          style={{marginLeft: 18, marginRight: 18, marginTop: 18}}
-          onPress={() => console.log('Link to Tax')}>
-          Tax
-        </Button>
-      </KeyboardAvoidingView>
-    </>
-  );
-};
+export default class FinanceScreen extends Component {
+  constructor() {
+    super();
+    this.Array_Items = array;
+  }
+  render() {
+    return (
+      <View style={styles.MainContainer}>
+        <ScrollView>
+          {this.Array_Items.map((item, key) => (
+            <View key={key} style={styles.item}>
+              <Text
+                style={styles.item_text_style}
+                onPress={() => {
+                  Alert.alert(item.toString());
+                }}>
+                {item}
+              </Text>
 
-export default FinanceScreen;
+              <View style={styles.item_separator} />
+            </View>
+          ))}
+        </ScrollView>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  MainContainer: {
+    flex: 1,
+    paddingTop: Platform.OS === 'ios' ? 20 : 0,
+  },
+
+  item_text_style: {
+    fontSize: 20,
+    color: '#000',
+    padding: 10,
+  },
+
+  item_separator: {
+    height: 1,
+    width: '100%',
+    backgroundColor: '#263238',
+  },
+});
